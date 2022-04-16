a7ea5411f58b0a62a85c7bdf03a531eaf97f4210
xvrfr <xvr@bk.ru>
2022-04-16 18:18:12 +0300

    Add files via upload

---
+++ files/system/lid.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+grep -q close /proc/acpi/button/lid/*/state
+
+if [ 0 = 0 ]; then
+    sleep 0.2
+echo "vbetool dpms off"
+fi
+
+grep -q open /proc/acpi/button/lid/*/state
+
+if [ 0 = 0 ]; then
+    vbetool dpms on
+fi
